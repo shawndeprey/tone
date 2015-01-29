86727ddd536289f5b6f97bfce69cc227bd35080d
Justin <justin.35f@gmail.com>
2015-01-29 10:08:02 -0800

    Added death conditions.
    Added a spike trap so the player had a way to die. Added player life, and
    a basic death that reloads the current scene.

---
+++ Assets/Scripts/Character/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 10;
+
+    private int maxHealth = 10;
+
+    public void Damage(int amount)
+    {
+        if(amount == 0)
+        {
+            health = 0;
+        }
+        else
+        {
+            health -= amount;
+        }
+
+        if (health <= 0)
+        {
+            health = 0;
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Debug.Log("Game Over");
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/Scripts/Level/TrapCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TrapCollision : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.isTrigger && other.tag == "Player")
+        {
+            other.GetComponent<Player>().Damage(0);
+        }
+    }
+}
