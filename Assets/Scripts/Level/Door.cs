a01207235901272bc2886a12d8e1edfd89831456
Shawn Deprey <shawndeprey@gmail.com>
2015-01-29 21:07:09 -0700

    WIP

---
+++ Assets/Scripts/Level/Door.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    public string sceneName;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player"){
+            Application.LoadLevel(sceneName);
+        }
+    }
+}
