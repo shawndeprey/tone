e77c8137a39d7679ccaccb513cb3e6e5a3100c7c
Shawn Deprey <shawndeprey@gmail.com>
2015-01-30 21:56:36 -0700

    Made player teleport to correct position when going through doors.

--- Assets/Scripts/Level/Door.cs
+++ Assets/Scripts/Level/Door.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
     public string sceneName;
+    public string doorName;
 
     void OnTriggerStay2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player"){
             if(Input.GetButton("Action")){
                 Application.LoadLevel(sceneName);
+                GameManager.Instance.SetMovingToDoor(doorName);
             }
         }
     }
