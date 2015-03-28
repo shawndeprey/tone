faa2c81a370697d3117c6bd8c559a1f0410bf471
Justin <justin.35f@gmail.com>
2015-03-28 10:37:39 -0700

    Added equipable items.
    Items can now be equipped and used. Passive items are separate from
    equippable items.

--- Assets/Scripts/Character/Player/PlayerInput.cs
+++ Assets/Scripts/Character/Player/PlayerInput.cs
@@ -9,6 +9,9 @@ public class PlayerInput : MonoBehaviour
     private Weapon primaryWeapon;
     private int currentWeaponIndex;
     private List<Weapon> weapons;
+    private Item primaryItem;
+    private int currentItemIndex;
+    private List<Item> items;
 
     void Start()
     {
@@ -23,6 +26,20 @@ public class PlayerInput : MonoBehaviour
 
         currentWeaponIndex = GameManager.Instance.equippedWeapon;
         EquipWeapon(currentWeaponIndex);
+
+
+
+        items = new List<Item>();
+        Item[] tempItems = GetComponents<Item>();
+
+        items.Add(null);
+        for (int i = 0; i < tempItems.Length; i++)
+        {
+            items.Add(tempItems[i]);
+        }
+
+        currentItemIndex = GameManager.Instance.equippedItem;
+        EquipItem(currentItemIndex);
     }
 
 	void Update()
@@ -48,6 +65,23 @@ public class PlayerInput : MonoBehaviour
             {
                 EquipNextWeapon();
             }
+
+            if (primaryItem != null)
+            {
+                if (Input.GetButton("Use Item"))
+                {
+                    primaryItem.UsePressed();
+                }
+                else if (Input.GetButtonUp("Use Item"))
+                {
+                    primaryItem.UseReleased();
+                }
+            }
+
+            if (Input.GetButtonDown("Item Swap"))
+            {
+                EquipNextItem();
+            }
         }
     }
 
@@ -65,5 +99,22 @@ public class PlayerInput : MonoBehaviour
         int count = GameManager.Instance.GetCurrentAmmo(weaponIndex);
         int max = GameManager.Instance.GetMaxAmmo(weaponIndex);
         MenuManager.Instance.GetWeaponDisplay().SetItem(weaponIndex, count, max);
+    }
+
+    public void EquipNextItem()
+    {
+        currentItemIndex = currentItemIndex >= items.Count - 1 ? 0 : currentItemIndex + 1;
+        GameManager.Instance.equippedItem = currentItemIndex;
+        EquipItem(currentItemIndex);
+    }
+
+    public void EquipItem(int itemIndex)
+    {
+        primaryItem = items[itemIndex];
+
+        int count = GameManager.Instance.GetCurrentCharges(itemIndex);
+        int max = GameManager.Instance.GetMaxCharges(itemIndex);
+        MenuManager.Instance.GetItemDisplay().SetItem(itemIndex, count, max);
+        Debug.Log(itemIndex);
     }
 }
---
+++ Assets/Scripts/Items/BasicItem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BasicItem : Item
+{
+    void Awake()
+    {
+        useRate = 0.5f;
+        useCooldown = 0f;
+    }
+
+    public override void Use(Vector2 direction)
+    {
+        if (CanUse)
+        {
+            useCooldown = useRate;
+        }
+    }
+
+    public override void UsePressed()
+    {
+        Vector2 direction = Vector2.zero;
+        direction.y = Input.GetAxis("Vertical");
+        direction.y = direction.y < 0f ? -1f : direction.y > 0f ? 1f : 0f;
+
+        if (direction.y == 0.0f)
+        {
+            direction.x = Input.GetAxis("Horizontal");
+            direction.x = direction.x < 0f ? -1f : direction.x > 0f ? 1f : 0f;
+            if (direction.x == 0.0f)
+            {
+                direction = gameObject.GetComponent<PlayerInput>().lastDirection;
+            }
+            else
+            {
+                gameObject.GetComponent<PlayerInput>().lastDirection = new Vector2(direction.x, gameObject.GetComponent<PlayerInput>().lastDirection.y);
+            }
+        }
+
+        Use(direction);
+    }
+
+    public override void UseReleased()
+    {
+        useCooldown = 0f;
+    }
+}
